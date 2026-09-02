ac793cfcbb344ad56cdad3296d1f379a647455e7
swat-mgr <swat-mgr@loop.local>
2026-09-02 18:37:27 +0800

    feat: detect supported database version packs

---
+++ apps/api/src/sqllens_api/evidence_connector/__init__.py
@@ -0,0 +1,15 @@
+from sqllens_api.evidence_connector.versioning import (
+    DatabaseProduct,
+    DetectedDatabaseVersion,
+    DetectionStatus,
+    VersionFingerprint,
+    detect_database_version,
+)
+
+__all__ = [
+    "DatabaseProduct",
+    "DetectedDatabaseVersion",
+    "DetectionStatus",
+    "VersionFingerprint",
+    "detect_database_version",
+]
